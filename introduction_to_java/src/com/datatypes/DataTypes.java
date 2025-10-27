01175e4b31ab44a02bae70de85004a6eea5a8e35
MURTHY <87651957+MURTHY5514@users.noreply.github.com>
2025-10-27 15:43:58 +0530

    updated code

---
+++ introduction_to_java/src/com/datatypes/DataTypes.java
@@ -0,0 +1,39 @@
+package com.datatypes;
+
+public class DataTypes {
+	// single line comment
+	/*
+	 * this is multi line comment
+	 */
+	public static void main(String[] args) {
+		/*
+		 * Data type : what kind of data we can store or use.
+		 * Two types:
+		 * ==========
+		 * 1. primitive 
+		 * 2. non - primitive
+		 * 
+		 * 1.Primitive:
+		 * ============
+		 * 1. byte  - -128 to 127
+		 * 2. short
+		 * 3. int
+		 * 4. long  - 120L
+		 * 5. float  - 2.7f
+		 * 6. double
+		 * 7. char
+		 * 8. boolean
+		 * 
+		 * 
+		 * 2. Non primitive:
+		 * =================
+		 * 1. Array - list of homogeneous data type elements
+		 * 2. String
+		 * 3. Class
+		 * 4. Interface
+		 */
+		byte age = 120;
+		System.out.println("Your age is: "+age);
+		
+	}
+}
