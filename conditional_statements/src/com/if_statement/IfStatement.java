b8cf9d9fe6b64367b16ae9b9dedeba410fe5ccf1
MURTHY <87651957+MURTHY5514@users.noreply.github.com>
2025-11-03 16:22:46 +0530

    updated code

--- conditional_statements/src/com/if_statement/IfStatement.java
+++ conditional_statements/src/com/if_statement/IfStatement.java
@@ -19,10 +19,13 @@ public class IfStatement {
 	 */
 	public static void main(String[] args) {
 		int num = 10;
-		if(num>10) {
-			System.out.println("Number is Greater than 10");
-		}else {
-			System.out.println("Number is not greater than 10");
+		if(num>10 && num <=20) {
+			System.out.println("Number is Greater than 10 and less than or equals to 20 ");
+		}else if(num>20 && num<=30) {
+			System.out.println("Number is Greater than 20 and less than or equals to 30 ");
+		}
+		else {
+			System.out.println("Number is not greater than 10 or greater then 30");
 		}
 	}
 }
